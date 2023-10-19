b8b282ef03227a22b807d585f9ef81ff636068c0
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-10-19 18:29:34 +0200

    first commit

---
+++ api/handler.ts
@@ -0,0 +1,9 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+ 
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+    console.log(request);
+    return response.redirect(307, 'https://appwrite.io');
+}
