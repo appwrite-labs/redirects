98d5e45b2a8e1e42025fe0d6e933e7764a794de4
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-10-19 18:36:55 +0200

    fix

--- api/handler.ts
+++ api/handler.ts
@@ -1,9 +1,19 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
- 
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
 export default function handler(
-  request: VercelRequest,
-  response: VercelResponse,
+    request: VercelRequest,
+    response: VercelResponse
 ) {
-    console.log(request);
-    return response.redirect(307, 'https://appwrite.io');
+    switch (request.headers["x-forwarded-host"]) {
+        case "appwrite.online":
+        case "is.appwrite.online":
+        case "status.appwrite.io":
+            return response.redirect(301, "https://www.appwrite.online");
+        case "careers.appwrite.io":
+            return response.redirect(301, "https://www.appwrite.careers");
+        case "storage.appwrite.io":
+            return response.redirect(301, "https://appwrite.store");
+        default:
+            return response.redirect(301, "https://www.appwrite.io");
+    }
 }
